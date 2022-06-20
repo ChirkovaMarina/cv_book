2d462e30ba118349f7a5e669a877544bc177c4a6
ChirkovaMarina <chirkova.m.1998@gmail.com>
2022-06-20 09:33:25 +0300

    first commit

---
+++ Module_II/task_5.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+class Better:
+    @staticmethod
+    def bt(self):
+        a = cv2.GaussianBlur(self, (11, 11), cv2.BORDER_DEFAULT)
+        a = cv2.addWeighted(self, 2.0, a, -1.0, 0, 1)
+        return a
+
+
+if __name__ == '__main__':
+    video_name = 'trm.168.091.avi'
+    cap = cv2.VideoCapture('../data/processing/' + video_name)
+    while cap.isOpened():
+        succeed, frame = cap.read()
+        if succeed:
+            frame = Better.bt(frame)
+            cv2.imshow(video_name, frame)
+        else:
+            cv2.destroyAllWindows()
+            cap.release()
+        cv2.waitKey(1)
